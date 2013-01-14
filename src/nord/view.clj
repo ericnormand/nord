5114151388aeb4e04b07f2001e140debaf356370
Eric Normand <eric@scrive.com>
2013-01-14 08:34:48 -0600

    Add Google Analytics tracking.

--- src/nord/view.clj
+++ src/nord/view.clj
@@ -55,6 +55,18 @@
     (include-js "/bootstrap/js/bootstrap.min.js")
     (include-js "/js/script.js")
     ;; put google analytics stuff here when you're ready
+    [:script "
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37632091-1']);
+  _gaq.push(['_setDomainName', 'nolaparks.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();"]
     ]))
 
 (defn app [pg & rst]
@@ -106,6 +118,18 @@
     (include-js "/bootstrap/js/bootstrap.min.js")
     (include-js "/js/script.js")
     ;; put google analytics stuff here when you're ready
+    [:script "
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37632091-1']);
+  _gaq.push(['_setDomainName', 'nolaparks.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();"]
     ]))
 
 (def features
